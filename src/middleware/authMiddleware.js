3007af5aa8518ad629b49dbfd00bd28c7e6a5182
karan-7058 <aghavkaran164@gmail.com>
2026-01-14 18:30:03 +0530

    created a require authentication middleware for checking user session , and check user logged in or not

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,9 @@
+const requireAuth = (req, res, next) => {
+    if (!req.session || !req.session.user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+    next();
+  };
+  
+  module.exports = { requireAuth };
+  
