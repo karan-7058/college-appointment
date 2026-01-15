209e2badd4c814e1982bba49dea8265e6961d36f
karan-7058 <aghavkaran164@gmail.com>
2026-01-15 11:04:19 +0530

    added a route definations for my appoinment and get availability for student

--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -2,11 +2,15 @@ const express=require('express');
 const router=express.Router();
 const {requireStudent}=require('../middleware/roleMiddleware.js');
 const{requireAuth}=require('../middleware/authMiddleware.js');
+const{getAvailability}=require('../controllers/availabilityController.js');
 
-const{bookAppointment}=require('../controllers/appointmentController.js');
+const{bookAppointment, myAppointment}=require('../controllers/appointmentController.js');
 
 
 router.post('/bookAppointment' , requireAuth, requireStudent, bookAppointment);
+router.get('/myAppointment' , requireAuth, requireStudent, myAppointment);
+router.get("/:professorId/availability" , requireAuth, getAvailability);
+
 
 module.exports=router;
 
