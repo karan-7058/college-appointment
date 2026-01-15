3a54ec2321a086828a522f89f3067b829220ce78
karan-7058 <aghavkaran164@gmail.com>
2026-01-15 11:02:47 +0530

    added a route defination for delete route

--- src/routes/professorRoutes.js
+++ src/routes/professorRoutes.js
@@ -2,22 +2,12 @@ const express = require("express");
 const router = express.Router();
 const { requireAuth } = require("../middleware/authMiddleware");
 const { requireProfessor } = require("../middleware/roleMiddleware");
-const {
-  createAvailability,
-  getAvailability,
-} = require("../controllers/availabilityController");
+const {createAvailability} = require("../controllers/availabilityController");
+const {deleteAppointment}=require('../controllers/appointmentController.js');
 
-router.post(
-  "/availability",
-  requireAuth,
-  requireProfessor,
-  createAvailability
-);
 
-router.get(
-  "/:professorId/availability",
-  requireAuth,
-  getAvailability
-);
+router.post("/availability", requireAuth, requireProfessor , createAvailability);
+
+router.delete('/appointment/:appointmentId', requireAuth, requireProfessor, deleteAppointment)
 
 module.exports = router;
